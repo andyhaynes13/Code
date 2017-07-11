e5b40f6a8b9a74d7bf20159dcb8f363cd096b676
andyhaynes13 <admin@haynescomputerservice.com>
2017-07-11 14:43:13 -0400

    div by 5and3

---
+++ div_by5.rb
@@ -0,0 +1,8 @@
+number=0
+100.times do
+		number=number+1
+if number % 5==0
+	p "mined"
+else p number
+end
+end
---
+++ div_by5and3.rb
@@ -0,0 +1,12 @@
+number=0
+100.times do
+		number=number+1
+if number % 15==0
+		p"minded Mines"
+
+elsif number % 3==0
+	p "minds"
+		
+else p number
+end
+end
